e3078cf111e0a9154e2b757ccd18cea41c540b5a
Moco2669 <moxtheboom004@gmail.com>
2025-03-26 20:54:08 +0100

    rover turns west

--- csharp/dotnet6/DotnetStarter.Logic.Tests/RoverTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/RoverTest.cs
@@ -11,12 +11,26 @@ public class RoverTest
         Rover rover = new();
         Assert.Equal("0:1:N", rover.ExecuteCommand("M"));
     }
+
+    [Fact]
+    public void RoverTurnsWest()
+    {
+        Rover rover = new();
+        Assert.Equal("0:0:W", rover.ExecuteCommand("L"));
+    }
 }
 
 public class Rover
 {
     public IEnumerable<char> ExecuteCommand(string s)
     {
+        switch (s)
+        {
+            case "M":
+                return "0:1:N";
+            case "L":
+                return "0:0:W";
+        }
         return "0:1:N";
     }
 }
