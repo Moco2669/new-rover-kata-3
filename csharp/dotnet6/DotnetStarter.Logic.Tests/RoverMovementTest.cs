d13664a7b1ee45f46d513b6ec76d52da3e0e953f
Moco2669 <moxtheboom004@gmail.com>
2025-03-27 20:33:31 +0100

    small test refactor

--- csharp/dotnet6/DotnetStarter.Logic.Tests/RoverMovementTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/RoverMovementTest.cs
@@ -4,17 +4,20 @@ namespace DotnetStarter.Logic.Tests;
 
 public class RoverMovementTest
 {
+    private const string move = "M";
+    private const string turnEastAndMove = "RM";
+    
     [Fact]
     public void RoverMovesNorth()
     {
         Rover rover = new();
-        Assert.Equal("0:1:N", rover.ExecuteCommand("M"));
+        Assert.Equal("0:1:N", rover.ExecuteCommand(move));
     }
 
     [Fact]
     public void RoverMovesEast()
     {
         Rover rover = new();
-        Assert.Equal("1:0:E", rover.ExecuteCommand("RM"));
+        Assert.Equal("1:0:E", rover.ExecuteCommand(turnEastAndMove));
     }
 }
