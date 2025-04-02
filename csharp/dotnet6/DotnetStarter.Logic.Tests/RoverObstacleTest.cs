4d9a45ebf167c083ce2e2139b1cf21f48cfb3267
Moco2669 <moxtheboom004@gmail.com>
2025-04-02 11:52:22 +0200

    rover moves to obstacle from south

--- csharp/dotnet6/DotnetStarter.Logic.Tests/RoverObstacleTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/RoverObstacleTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using Xunit;
 
 namespace DotnetStarter.Logic.Tests;
@@ -6,16 +7,29 @@ public class RoverObstacleTest
 {
     private const int obstacleXPosition = 4;
     private const int obstacleYPosition = 5;
+    private const int gridXSize = 7;
+    private const int gridYSize = 8;
     private const string obstacleIndication = "O:";
 
     private readonly string moveToObstacleFromNorth = new CommandBuilder().TurnRight().MoveTimes(obstacleXPosition)
         .TurnLeft().MoveTimes(obstacleYPosition - 1).Build();
+    private readonly string moveToObstacleFromSouth = new CommandBuilder().TurnRight().MoveTimes(obstacleXPosition).TurnRight().MoveTimes(gridYSize - obstacleYPosition).Build();
     private readonly string obstaclePositionFromNorth = obstacleIndication + obstacleXPosition + ":" + (obstacleYPosition - 1) + ":" + new North();
+
+    private readonly string obstaclePositionFromSouth =
+        obstacleIndication + obstacleXPosition + ":" + (obstacleYPosition + 1) + ":" + new South();
     
     [Fact]
     public void RoverReportsObstacleFromNorth()
     {
-        Rover rover = RoverBuilder.CreateRover().AddObstacle(obstacleXPosition, obstacleYPosition);
+        Rover rover = RoverBuilder.CreateRover(gridXSize, gridYSize).AddObstacle(obstacleXPosition, obstacleYPosition);
         Assert.Equal(obstaclePositionFromNorth, rover.ExecuteCommand(moveToObstacleFromNorth));
+    }
+
+    [Fact]
+    public void RoverReportsObstacleFromSouth()
+    {
+        Rover rover = RoverBuilder.CreateRover(gridXSize, gridYSize).AddObstacle(obstacleXPosition, obstacleYPosition);
+        Assert.Equal(obstaclePositionFromSouth, rover.ExecuteCommand(moveToObstacleFromSouth));
     }
 }
