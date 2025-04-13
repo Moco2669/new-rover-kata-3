17dd79755f13b59c77824280d747905cc20fd8d2
Moco2669 <moxtheboom004@gmail.com>
2025-04-13 16:27:23 +0200

    small test refactoring
    rover is setup before each test and is easier to read

--- csharp/dotnet6/DotnetStarter.Logic.Tests/RoverOrientationTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/RoverOrientationTest.cs
@@ -6,59 +6,58 @@ namespace DotnetStarter.Logic.Tests;
 
 public class RoverOrientationTest
 {
+    private Rover _rover;
+
+    public RoverOrientationTest()
+    {
+        _rover = RoverBuilder.CreateRover();
+    }
+    
     [Fact]
     public void RoverTurnsWest()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:W", rover.ExecuteCommand("L"));
+        Assert.Equal("0:0:W", _rover.ExecuteCommand("L"));
     }
 
     [Fact]
     public void RoverTurnsSouth()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:S", rover.ExecuteCommand("LL"));
+        Assert.Equal("0:0:S", _rover.ExecuteCommand("LL"));
     }
 
     [Fact]
     public void RoverTurnsEastTurningRight()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:E", rover.ExecuteCommand("R"));
+        Assert.Equal("0:0:E", _rover.ExecuteCommand("R"));
     }
 
     [Fact]
     public void RoverWrapsAroundTurningLeft()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:N", rover.ExecuteCommand("LLLL"));
+        Assert.Equal("0:0:N", _rover.ExecuteCommand("LLLL"));
     }
 
     [Fact]
     public void RoverFacesSouthTurningRight()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:S", rover.ExecuteCommand("RR"));
+        Assert.Equal("0:0:S", _rover.ExecuteCommand("RR"));
     }
 
     [Fact]
     public void RoverFacesWestTurningRight()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:W", rover.ExecuteCommand("RRR"));
+        Assert.Equal("0:0:W", _rover.ExecuteCommand("RRR"));
     }
 
     [Fact]
     public void RoverWrapsAroundTurningRight()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:N", rover.ExecuteCommand("RRRR"));
+        Assert.Equal("0:0:N", _rover.ExecuteCommand("RRRR"));
     }
 
     [Fact]
     public void RoverFacesEastTurningLeft()
     {
-        Rover rover = RoverBuilder.CreateRover();
-        Assert.Equal("0:0:E", rover.ExecuteCommand("LLL"));
+        Assert.Equal("0:0:E", _rover.ExecuteCommand("LLL"));
     }
 }
